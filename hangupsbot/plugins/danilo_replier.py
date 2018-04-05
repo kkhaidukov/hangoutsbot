fa89ce620b8c7ee2b88f36ab0eadcde4a78b077d
Kirill Khaidukov <kkhaidukov@newstore.com>
2018-04-05 11:34:44 +0100

    will prepend stuff to the sentence to hide the 'relevant' keyword; shuffle images from image search; track dialog length to respond without the 'normal' triggers

--- hangupsbot/plugins/danilo_replier.py
+++ hangupsbot/plugins/danilo_replier.py
@@ -1,3 +1,4 @@
+from collections import deque
 import os
 import logging
 import random
@@ -9,9 +10,14 @@ from nltk import pos_tag, word_tokenize
 import requests
 
 MESSAGES_FILE = '%s/danilo_messages.txt' % os.path.dirname(os.path.realpath(__file__))
-SENTENCE_LENGTH_LIMIT = 20
+SENTENCE_LENGTH_LIMIT = 10
+PREFIX_LENGTH_LIMIT = 5
 ALPHABET = 'йцукенгшщзххъёфывапролджэячсмитьбю'
 LINE_START = '~'
+IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif']
+IMAGE_REQUEST_PREFIX = 'данило, картинку:'
+# actually not length, but probability that a conversation will continue
+MAX_DIALOG_LENGTH = 10
 
 logger = logging.getLogger(__name__)
 rs = RussianStemmer()
@@ -23,10 +29,14 @@ class Replier(object):
         self.trigrams_dict = {}
         # {stem: (LINE_START, full_word),}
         self.initials = {}
+        self.inverted_trigrams = {}
+        self.trigrams = []
+        self.current_dialog_length = 0
 
         self.init_messages()
         self.init_ngrams()
         self.init_initials()
+        self.init_inverted_trigrams()
 
     def clean_word(self, word):
         return ''.join(ch for ch in word if ch.isalnum() or ch == u'-')
@@ -49,21 +59,32 @@ class Replier(object):
                 raw += '%s %s\n' % (LINE_START, line)
 
         tokens = nltk.word_tokenize(raw)
-        trigrams = nltk.trigrams(tokens)
-        for tr in trigrams:
+        self.trigrams = list(nltk.trigrams(tokens))
+        for tr in self.trigrams:
             self.trigrams_dict.setdefault((tr[0], tr[1]), []).append(tr[2])
         logger.info('len(self.trigrams_dict.keys()) = %s' % len(self.trigrams_dict.keys()))
 
     def init_initials(self):
+        logger.info("INIT_INITIALS")
         for k in self.trigrams_dict.keys():
             if k[0] == LINE_START:
                 self.initials[rs.stem(k[1])] = k
         logger.info('len(self.initials) = %s' % len(self.initials))
 
+    def init_inverted_trigrams(self):
+        """
+        dict of inverted trigrams to be used for prepending
+        turns a trigram (a, b, c) into a dict of {(b, c): a,}
+        """
+        logger.info("INIT_INVERTED_TRIGRAMS")
+        for a, b, c in self.trigrams:
+            self.inverted_trigrams.setdefault((b, c), []).append(a)
+        logger.info('len(self.inverted_trigrams) = %s' % len(self.inverted_trigrams))
+
     def _has_image_link(self, message):
         if 'http' not in message:
             return False
-        for ext in ['jpg', 'png', 'gif']:
+        for ext in IMAGE_EXTENSIONS:
             if ext in message:
                 return True
         else:
@@ -86,20 +107,53 @@ class Replier(object):
         if relevant_message:
             return self.generate_message(relevant_message)
 
+    def should_continue_dialog(self):
+        logger.info(
+            'Determining if a the dialog should continue, self.current_dialog_length = %s' % self.current_dialog_length)
+        # in general, it should not start a dialog based on this randomization
+        if self.current_dialog_length == 0:
+            return False
+
+        # the probability of a consecutive response will diminish with every other message
+        return random.randrange(10) < MAX_DIALOG_LENGTH - self.current_dialog_length
+
     def get_response(self, message):
+        # determine if it even wants to continue the conversation
+        will_continue_dialog = self.should_continue_dialog()
+        logger.info('will_continue_dialog = %s' % will_continue_dialog)
+
+        response = None
         if self._has_image_link(message):
-            return self.get_response_to_an_image(message)
+            response = self.get_response_to_an_image(message)
         # fairly rarely try to be relevant
-        elif random.randrange(10) > 8:
-            return self.generate_message_with_pos_tagging(message)
+        elif will_continue_dialog or random.randrange(10) > 8:
+            # but if it's not relevant then we'll drop a general generated message
+            response = self.generate_message_with_pos_tagging(message) or self.generate_message()
         # sometimes respond with a picture
-        elif random.randrange(10) > 8:
-            return self.get_image_url_response(message)
-        elif 'данило' in message.lower():
-            return self.generate_message()
+        elif will_continue_dialog or random.randrange(10) > 8 or message.startswith(IMAGE_REQUEST_PREFIX):
+            response = self.get_image_url_response(message.replace(IMAGE_REQUEST_PREFIX, '').strip())
+        elif will_continue_dialog or 'данило' in message.lower():
+            if random.choice([0, 1]):
+                response = self.generate_message()
+            else:
+                response = self.generate_message_with_pos_tagging(message)
+        if response:
+            # the probability of another response should decrease with every message added
+            self.current_dialog_length += 1
+
+        # don't let it get too low
+        if self.current_dialog_length > 5:
+            self.current_dialog_length = 0
+
+        return response
+
+    def sentence_appendleft(self, sentence, prefix):
+        d = deque(sentence)
+        d.appendleft(prefix)
+        return list(d)
 
     def generate_message(self, initial=None):
-        logger.info('Using generate_message to create a response.')
+        logger.info('Using generate_message to create a response, initial = %s' % str(initial))
         if initial:
             if type(initial) == str:
                 sentence = [w.strip().lower() for w in initial.split()]
@@ -113,16 +167,41 @@ class Replier(object):
             next_ = random.choice(self.trigrams_dict.get((one, two)))
             if not next_:
                 break
-            elif i > SENTENCE_LENGTH_LIMIT and next_.strip() in ['.', '!', '?']:
+            elif i > SENTENCE_LENGTH_LIMIT and next_.strip() in ['!', '?']:
                 sentence.append(next_)
+                i += 1
                 break
-            elif i > SENTENCE_LENGTH_LIMIT and next_.strip() == ',':
+            elif i > SENTENCE_LENGTH_LIMIT and next_.strip() in ['.', ',']:
                 break
             elif next_:
                 sentence.append(next_)
                 i += 1
             else:
                 break
+        # flip a coin to prepend
+        if random.randrange(2):
+            logger.info('Prepending the sentence, sentence so far: %s' % sentence)
+            prefix_length = 0
+            debug_prefix_deque = deque()
+            while True:
+                logger.info('sentence[:2]: %s' % sentence[:2])
+                # logger.info('tuple(sentence[:2]): %s' % tuple(sentence[:2]))
+                suffix = tuple(sentence[:2])
+                logger.info('Suffix: %s' % str(suffix))
+                prefix = random.choice(self.inverted_trigrams.get(suffix, []))
+                logger.info('Got prefix: %s' % prefix)
+                if prefix:
+                    if prefix == LINE_START and prefix_length > PREFIX_LENGTH_LIMIT:
+                        sentence = self.sentence_appendleft(sentence, prefix)
+                        debug_prefix_deque.appendleft(prefix)
+                        break
+                    else:
+                        sentence = self.sentence_appendleft(sentence, prefix)
+                        debug_prefix_deque.appendleft(prefix)
+                        prefix_length += 1
+                else:
+                    break
+            logger.info('Prepended prefix: %s' % debug_prefix_deque)
 
         sentence_string = ''
         for w in sentence:
@@ -158,14 +237,15 @@ class Replier(object):
                 if initial:
                     return self.generate_message(initial)
         else:
-            logger.info("Could not generate anything from message '%s'" % message)
+            logger.info("Could not get S, V from message '%s'" % message)
+        logger.info("Could not generate anything from message '%s'" % message)
 
     def get_image_url_response(self, message):
         logger.info('Using get_image_url_response to respond to "%s"' % message)
         tagged = pos_tag(word_tokenize(message), lang='rus')
         s_v = [word for word, key in tagged if key in ['S', 'V']]
         random.shuffle(s_v)
-        base_url = 'https://api.qwant.com/api/search/images?count=1&offset=1&q=%s'
+        base_url = 'https://api.qwant.com/api/search/images?count=10&offset=1&q=%s'
         while s_v:
             q = quote_plus(' '.join(s_v))
             url = base_url % q
@@ -174,7 +254,14 @@ class Replier(object):
             logger.info('Calling image search URL: %s' % url)
             result = requests.get(url, headers=headers).json()
             if result['data']['result']['total']:
-                return result['data']['result']['items'][0]['media']
+                media_urls = [item['media'] for item in result['data']['result']['items']]
+                random.shuffle(media_urls)
+                for media_url in media_urls:
+                    for ext in IMAGE_EXTENSIONS:
+                        if media_url.endswith('.%s' % ext):
+                            return media_url
+                    else:
+                        logger.warning('None of the URLs in the search results contained a proper image')
             else:
                 logger.info('No results, will retry with a smaller query')
                 s_v.pop()
